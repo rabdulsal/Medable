331153c53d6603dcf8230ef5f085b4124e7a3eae
Rashad Abdul-Salaam <rashad.abdul-salaam@uphs.upenn.edu>
2018-04-18 16:21:49 -0400

    Medable Cocoapod configured and wired up

---
+++ MedableSandbox-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  MedableSandbox-Bridging-Header.h
+//  MedableSandbox
+//
+//  Created by Rashad Abdul-Salam on 4/18/18.
+//  Copyright © 2018 University of Pennsylvania Health System. All rights reserved.
+//
+
+#ifndef MedableSandbox_Bridging_Header_h
+#define MedableSandbox_Bridging_Header_h
+
+#import <Medable/Medable.h>
+
+#endif /* MedableSandbox_Bridging_Header_h */
