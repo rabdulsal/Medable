e01e7b17a4a092b20c3e870a028932c6ff4a8d35
Rashad Abdul-Salaam <rashad.abdul-salaam@uphs.upenn.edu>
2018-04-19 09:56:43 -0400

    Update Pod structure + gitignore

--- Pods/Target Support Files/Pods-MedableSandboxTests/Pods-MedableSandboxTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MedableSandboxTests : NSObject
-@end
-@implementation PodsDummy_Pods_MedableSandboxTests
-@end
--- Pods/Target Support Files/Pods-MedableSandboxUITests/Pods-MedableSandboxUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MedableSandboxUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MedableSandboxUITests
-@end
